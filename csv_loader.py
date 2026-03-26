84ed739d305afa61d38a13f9b0922b38c30a9281
aylatabi <ayta5739@colorado.edu>
2026-03-26 18:32:26 -0400

    Initial Commit for 3/26

---
+++ csv_loader.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#read csv file and do error checking to see if 
+def read_command_line():
+    pass
+# Read a CSV file 
+def load_csv(filepath):
+    pass
+
+def insert_data():
+    pass
+
