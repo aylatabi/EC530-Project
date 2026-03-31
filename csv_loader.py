9a0c0f4cae2cfded64ed866762999197e465b2d1
aylatabi <ayta5739@colorado.edu>
2026-03-31 14:34:36 -0400

    Added creating sql string for building table

--- csv_loader.py
+++ csv_loader.py
@@ -21,11 +21,27 @@ def map_dtypes(df):
             results[col_name] = "TEXT"
     return results
 
+def build_create_table(table_name, table_dict):
+    sql_table_name = "CREATE TABLE " + table_name
+    
+    columns = ["id INTEGER PRIMARY KEY AUTOINCREMENT"]
+    
+    for key, value in table_dict.items():
+        columns.append(key + " " + value)
+    
+    sql_table_data = ", ".join(columns)
+    sql_string = sql_table_name + "(" + sql_table_data + ");"
+    return sql_string
+
+# def build_sql_table(sql_string):
+    
 def main():
     
     df = load_csv("locations.csv")
     results = map_dtypes(df)
     print(results)
+    build_create_table("locations", results)
+    
     return 0
 
 
--- pytest_project.py
+++ pytest_project.py
@@ -1,16 +1,20 @@
 import pandas as pd
-from csv_loader import load_csv, map_dtypes
+from csv_loader import load_csv, map_dtypes, build_create_table
 df = pd.read_csv("locations.csv")
-
+test_dict = {'Location': 'TEXT', 'Latitude / Longitude': 'TEXT'}
 #python -m pytest pytest_project.py 
 def test_df_creation():  
     result = load_csv("locations.csv")
     assert df["Location"][0] == result["Location"][0]
     
 def test_df_mapping():
-   test_dict = {'Location': 'TEXT', 'Latitude / Longitude': 'TEXT'}
    result =  map_dtypes(df)
    assert test_dict["Location"] == result["Location"]
+   
+def test_create_table_sql():
+    test_sql_string = "CREATE TABLE locations(id INTEGER PRIMARY KEY AUTOINCREMENT, Location TEXT, Latitude / Longitude TEXT);"
+    result = build_create_table("locations", test_dict) 
+    assert test_sql_string == result
 # def test_bad_coordinates_latitude_1():
 #     result = find_closest_airport((-200, -97.7431), None, False, df)
 #     assert result == -1
