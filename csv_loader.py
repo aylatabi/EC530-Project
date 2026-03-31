aae97c5278544159cc686701079026f4f6e3b6b1
aylatabi <ayta5739@colorado.edu>
2026-03-31 14:15:52 -0400

    added df mapping

--- csv_loader.py
+++ csv_loader.py
@@ -1,16 +1,31 @@
 import sys
 import pandas as pd
+from enum import Enum
+
 
 
 def load_csv(file_path):
     return pd.read_csv(file_path)    
-    
-    
+
+def map_dtypes(df):
+    results = {}  
+    for col_name in df.columns:
+        dtype = df[col_name].dtype
+        if dtype == "str":
+            results[col_name] = "TEXT"  
+        elif dtype == "int64":
+            results[col_name] = "INTEGER"  
+        elif dtype == "float64":
+            results[col_name] = "REAL"
+        else:
+            results[col_name] = "TEXT"
+    return results
+
 def main():
     
     df = load_csv("locations.csv")
-    
-    print(df["Location"][0])
+    results = map_dtypes(df)
+    print(results)
     return 0
 
 
--- pytest_project.py
+++ pytest_project.py
@@ -1,11 +1,16 @@
 import pandas as pd
-from csv_loader import load_csv
+from csv_loader import load_csv, map_dtypes
 df = pd.read_csv("locations.csv")
 
-def test_austin_airport():  
+#python -m pytest pytest_project.py 
+def test_df_creation():  
     result = load_csv("locations.csv")
     assert df["Location"][0] == result["Location"][0]
     
+def test_df_mapping():
+   test_dict = {'Location': 'TEXT', 'Latitude / Longitude': 'TEXT'}
+   result =  map_dtypes(df)
+   assert test_dict["Location"] == result["Location"]
 # def test_bad_coordinates_latitude_1():
 #     result = find_closest_airport((-200, -97.7431), None, False, df)
 #     assert result == -1
